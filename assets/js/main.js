92f765f06f704b7c8cb5c5313cafd6600e711951
malkio <coding.malkio@gmail.com>
2016-04-17 14:39:08 +0800

    Version 1.0.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,61 @@
 (function($, window, document, undefined){
+ function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
 
             var workspace = {
             	initialize: function() {
 
 		        	interact('.user-image')
-  .draggable({
-    onmove: window.dragMoveListener
-  })
-  .resizable({
-    preserveAspectRatio: true,
-    edges: { left: true, right: true, bottom: true, top: true }
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
+						  .draggable({
+						    onmove: window.dragMoveListener
+						  })
+						  .resizable({
+						    preserveAspectRatio: true,
+						    edges: { left: true, right: true, bottom: true, top: true }
+						  })
+						  .on('resizemove', function (event) {
+						    var target = event.target,
+						        x = (parseFloat(target.getAttribute('data-x')) || 0),
+						        y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
+						    // update the element's style
+						    target.style.width  = event.rect.width + 'px';
+						    target.style.height = event.rect.height + 'px';
 
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
+						    // translate when resizing from top or left edges
+						    x += event.deltaRect.left;
+						    y += event.deltaRect.top;
 
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
+						    target.style.webkitTransform = target.style.transform =
+						        'translate(' + x + 'px,' + y + 'px)';
 
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
-  });
+						    target.setAttribute('data-x', x);
+						    target.setAttribute('data-y', y);
+						    target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+						  });
             	},
             	eraserStart: function(){
-            			$('.user-image').eraser({
-		          		progressFunction: function(p) {
-		            		$('#progress').html(Math.round(p*100)+'%');
-		          		}
-		        	});
+        			$('.user-image').eraser({
+	          			progressFunction: function(p) {
+	            			$('#progress').html(Math.round(p*100)+'%');
+	          			}
+	        		});
         			$('.user-image').eraser('enable');
             	},
             	eraserEnd: function(){
@@ -65,6 +83,7 @@
             	move = false;
             	workspace.eraserEnd();
             	crop = false;
+            	$(".btn-group .btn").removeClass('active');
             }
  			$('.btn-move').on('click', function(){
  				if( !move ) {
@@ -94,6 +113,6 @@
  			});
 
 // this is used later in the resizing and gesture demos
-//  window.dragMoveListener = dragMoveListener;
+
 
 }(jQuery, window, document, undefined));
